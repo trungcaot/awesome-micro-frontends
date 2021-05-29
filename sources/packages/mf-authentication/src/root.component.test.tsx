cc840577dd6d3b7b535c789691cebd803193cdd7
Trung Cao Thanh <caothanhtrung4193@gmail.com>
2021-05-29 16:06:19 +0700

    fix display error for toBeInTheDocument.

--- sources/packages/mf-authentication/src/root.component.test.tsx
+++ sources/packages/mf-authentication/src/root.component.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Root from './root.component';
+import '@testing-library/jest-dom';
 
 describe('Root component', () => {
   it('should be in the document', () => {
