6e8d9da59c8b272ac444552e38458eae2e7e3d82
Trung Cao Thanh <caothanhtrung4193@gmail.com>
2021-05-29 15:45:28 +0700

    config mf-authentication route to root-config

--- sources/packages/root-config/src/demo-root-config.ts
+++ sources/packages/root-config/src/demo-root-config.ts
@@ -3,7 +3,7 @@ import { registerApplication, start } from 'single-spa';
 registerApplication({
   name: '@demo/mf-authentication',
   app: () => System.import('@demo/mf-authentication'),
-  activeWhen: ['/auth'],
+  activeWhen: ['/'],
 });
 
 start({
