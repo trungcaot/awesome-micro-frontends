9b1fd3f19a0ede33ee0a692703c77266a1715c6b
Trung Cao Thanh <caothanhtrung4193@gmail.com>
2021-05-29 13:58:56 +0700

    added typescript-eslint

--- sources/packages/root-config/src/demo-root-config.ts
+++ sources/packages/root-config/src/demo-root-config.ts
@@ -2,10 +2,7 @@ import { registerApplication, start } from 'single-spa';
 
 registerApplication({
   name: '@single-spa/welcome',
-  app: () =>
-    System.import(
-      'https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js'
-    ),
+  app: () => System.import('https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js'),
   activeWhen: ['/'],
 });
 
