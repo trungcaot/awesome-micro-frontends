9109f71f4284a1f2cbf138abcb120807a2a03dc1
Trung Cao Thanh <caothanhtrung4193@gmail.com>
2021-05-29 14:59:35 +0700

    init source code for mf-authentication

--- sources/packages/root-config/src/demo-root-config.ts
+++ sources/packages/root-config/src/demo-root-config.ts
@@ -1,16 +1,10 @@
 import { registerApplication, start } from 'single-spa';
 
 registerApplication({
-  name: '@single-spa/welcome',
-  app: () => System.import('https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js'),
-  activeWhen: ['/'],
+  name: '@demo/mf-authentication',
+  app: () => System.import('@demo/mf-authentication'),
+  activeWhen: ['/auth'],
 });
-
-// registerApplication({
-//   name: "@demo/navbar",
-//   app: () => System.import("@demo/navbar"),
-//   activeWhen: ["/"]
-// });
 
 start({
   urlRerouteOnly: true,
